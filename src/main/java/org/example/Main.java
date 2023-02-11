eaddb6e8f064e822dd932a6fec953056b1dc2d3d
Dianakyz <3222777my@gmail.com>
2023-02-11 21:14:58 +0300

    написала тесты

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,20 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UserNonUniqueException {
+
+        UserRepository userRepository = new UserRepository();
+        UserService userService = new UserService(userRepository);
+
+        userService.addNewUser("Ann", "test1");
+        userService.addNewUser("Masha", "test222");
+        userService.addNewUser("Tom", "12345");
+
+        //userService.addNewUser("Ann", "g kfjnd"); // для проверки работы исключения
+        //userService.addNewUser("Ann", "jdszfxghjk"); // для проверки работы исключения
+
+
+        System.out.println(userRepository.getAllUsers());
+        System.out.println(userService.getAllUsersLogins());
     }
 }
